a27a265ccb5ef77f1e57fe452766a8a0385d6ec0
iluhinoBruho <ix2.evdokimov@gmail.com>
2024-11-03 17:36:56 +0300

    AI generator generation limitations

--- src/main/java/ru/mipt/bit/platformer/logics/models/Tank.java
+++ src/main/java/ru/mipt/bit/platformer/logics/models/Tank.java
@@ -3,6 +3,8 @@ package ru.mipt.bit.platformer.logics.models;
 import ru.mipt.bit.platformer.util.Vector2D;
 
 
+import java.util.Objects;
+
 import static com.badlogic.gdx.math.MathUtils.isEqual;
 import static com.badlogic.gdx.math.MathUtils.random;
 import static ru.mipt.bit.platformer.util.GdxGameUtils.continueProgress;
@@ -119,5 +121,18 @@ public class Tank implements GameObject, Livable, Movable {
     @Override
     public float getRelativeHealth() {
         return curHealth / fullHealth;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tank tank = (Tank) o;
+        return Float.compare(motionProgress, tank.motionProgress) == 0 && Float.compare(MOVEMENT_SPEED, tank.MOVEMENT_SPEED) == 0 && Float.compare(bulletMovementSpeed, tank.bulletMovementSpeed) == 0 && Float.compare(damagePerBullet, tank.damagePerBullet) == 0 && Float.compare(curHealth, tank.curHealth) == 0 && Float.compare(fullHealth, tank.fullHealth) == 0 && direction == tank.direction && Objects.equals(coordinates, tank.coordinates) && Objects.equals(destCoordinates, tank.destCoordinates);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(direction, coordinates, destCoordinates, motionProgress, MOVEMENT_SPEED, bulletMovementSpeed, damagePerBullet, curHealth, fullHealth);
     }
 }
