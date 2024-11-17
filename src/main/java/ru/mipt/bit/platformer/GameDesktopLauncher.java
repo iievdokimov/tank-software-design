baa944a00a542e954b0d5746a37952107163fa81
iluhinoBruho <ix2.evdokimov@gmail.com>
2024-11-17 20:15:06 +0300

    AppContext-beans for: Level, GameDesktopLauncher but not for Drawer.

--- src/main/java/ru/mipt/bit/platformer/GameDesktopLauncher.java
+++ src/main/java/ru/mipt/bit/platformer/GameDesktopLauncher.java
@@ -10,55 +10,56 @@ import static com.badlogic.gdx.graphics.GL20.GL_COLOR_BUFFER_BIT;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.ObjectProvider;
 import ru.mipt.bit.platformer.logics.action_generators.AITanksActionsGenerator;
 import ru.mipt.bit.platformer.logics.action_generators.ActionsGenerator;
 import ru.mipt.bit.platformer.logics.action_generators.BulletActionsGenerator;
 import ru.mipt.bit.platformer.logics.action_generators.PlayerActionsGenerator;
 import ru.mipt.bit.platformer.logics.actions.Action;
 import ru.mipt.bit.platformer.logics.level_setup.LevelProvider;
-import ru.mipt.bit.platformer.logics.level_setup.RandomLevelProvider;
 import ru.mipt.bit.platformer.logics.models.Level;
 import ru.mipt.bit.platformer.util.Vector2D;
 import ru.mipt.bit.platformer.visuals.*;
-import ru.mipt.bit.platformer.visuals.configuration.AppConfig;
+import ru.mipt.bit.platformer.configuration.AppConfig;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
+@Component
 public class GameDesktopLauncher implements ApplicationListener {
+    //@Autowired
+    //private ObjectProvider<Drawer> drawerProvider;
 
+    //@Autowired
     private Drawer drawer;
 
+    @Autowired
     private Level level;
 
-    private ArrayList<ActionsGenerator> actionGenerators;
+    @Autowired
+    private List<ActionsGenerator> actionGenerators;
 
-    private final ApplicationContext context;
 
-
-    public GameDesktopLauncher(ApplicationContext context){
+    public GameDesktopLauncher(){
         super();
-        this.context = context;
     }
 
 
     @Override
     public void create() {
-        LevelProvider levelProvider = context.getBean(LevelProvider.class);
-        this.level = levelProvider.getLevel();
+        // Drawer-bean not working cause of LibGDX context needed, how (?)
+        // drawer = context.getBean(Drawer.class);
+        // drawer = drawerProvider.getIfAvailable();
 
-        HealthBarSettings healthBarSettings = context.getBean(HealthBarSettings.class);
+        //HealthBarSettings healthBarSettings = context.getBean(HealthBarSettings.class);
+        HealthBarSettings healthBarSettings = new HealthBarSettings(true);
 
-        // context.registerBean(Drawer.class, () -> new GdxDrawer(level, healthBarSettings));
-
-        // can't create drawer before levelProvider creates it (?)
         drawer = new GdxDrawer(level, healthBarSettings);
-        level.subscribe(drawer);
 
-        actionGenerators = new ArrayList<>();
-        actionGenerators.add(new PlayerActionsGenerator(level, healthBarSettings));
-        actionGenerators.add(new AITanksActionsGenerator(level));
-        actionGenerators.add(new BulletActionsGenerator(level)); // -> to Level:updateProgress
+        level.subscribe(drawer);
     }
 
     @Override
@@ -110,6 +111,6 @@ public class GameDesktopLauncher implements ApplicationListener {
         config.setWindowedMode((int)(squareTileWidth * levelSize.x()), (int)(squareTileWidth * levelSize.y()));
         // TODO: generate new level.tmx files for bigger than 8x10 levels (?)
 
-        new Lwjgl3Application(new GameDesktopLauncher(context), config);
+        new Lwjgl3Application(context.getBean(GameDesktopLauncher.class), config);
     }
 }
--- src/main/java/ru/mipt/bit/platformer/configuration/AppConfig.java
+++ src/main/java/ru/mipt/bit/platformer/configuration/AppConfig.java
@@ -1,7 +1,12 @@
-package ru.mipt.bit.platformer.visuals.configuration;
+package ru.mipt.bit.platformer.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import ru.mipt.bit.platformer.GameDesktopLauncher;
+import ru.mipt.bit.platformer.logics.action_generators.AITanksActionsGenerator;
+import ru.mipt.bit.platformer.logics.action_generators.ActionsGenerator;
+import ru.mipt.bit.platformer.logics.action_generators.BulletActionsGenerator;
+import ru.mipt.bit.platformer.logics.action_generators.PlayerActionsGenerator;
 import ru.mipt.bit.platformer.logics.level_setup.LevelProvider;
 import ru.mipt.bit.platformer.logics.level_setup.RandomLevelProvider;
 import ru.mipt.bit.platformer.logics.models.Level;
@@ -9,6 +14,9 @@ import ru.mipt.bit.platformer.util.Vector2D;
 import ru.mipt.bit.platformer.visuals.Drawer;
 import ru.mipt.bit.platformer.visuals.GdxDrawer;
 import ru.mipt.bit.platformer.visuals.HealthBarSettings;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class AppConfig {
@@ -37,13 +45,14 @@ public class AppConfig {
     }
 
     @Bean
-    public LevelProvider levelProvider() {
-        return new RandomLevelProvider(
-                new Vector2D(0, 0),
-                new Vector2D(7, 7),
-                0.1f, 4
-        );
+    public Level level(LevelProvider levelProvider) {
+        return levelProvider.getLevel(); // singleton creation guarantied
     }
+
+//    @Bean
+//    public Drawer drawer(Level level, HealthBarSettings healthBarSettings) {
+//        return new GdxDrawer(level, healthBarSettings);
+//    }
 
     @Bean
     public HealthBarSettings healthBarSettings(){
@@ -51,12 +60,21 @@ public class AppConfig {
     }
 
     @Bean
+    public List<ActionsGenerator> actionGenerators(Level level, HealthBarSettings healthBarSettings) {
+        List<ActionsGenerator> generators = new ArrayList<>();
+        generators.add(new PlayerActionsGenerator(level, healthBarSettings));
+        generators.add(new AITanksActionsGenerator(level));
+        generators.add(new BulletActionsGenerator(level));
+        return generators;
+    }
+
+    @Bean
+    public GameDesktopLauncher gameDesktopLauncher() {
+        return new GameDesktopLauncher();
+    }
+
+    @Bean
     public int squareTileWidth(){
         return 128;
     }
-
-//    @Bean
-//    public Drawer drawer(Level level, HealthBarSettings healthBarSettings) {
-//        return new GdxDrawer(level, healthBarSettings);
-//    }
 }
